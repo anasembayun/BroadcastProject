f6fc68fca371ae84059722eb5db66e6c525ea7ad
anasembayun <anasembayun02@mail.ugm.ac.id>
2022-02-18 12:31:59 +0700

    broadcast

---
+++ app/src/main/java/com/example/broadcastproject/CustomReceiver.java
@@ -0,0 +1,30 @@
+package com.example.broadcastproject;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class CustomReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        String msg = "This is default message";
+        switch (intent.getAction()){
+            case Intent.ACTION_POWER_CONNECTED:
+                msg = "The power is connected";
+                break;
+            case Intent.ACTION_POWER_DISCONNECTED:
+                msg = "The power is disconnected";
+                break;
+            case "ACTION_CUSTOM_BROADCAST":
+                /* msg = "This is custom broadcast"; */
+                msg = intent.getStringExtra("Data");
+            default:
+                break;
+        }
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+}
